02c9e84f352ebd7831bb2bd6fb9e5469f1cf2471
Michal Repčík <148249444+rm-a0@users.noreply.github.com>
2024-04-25 16:37:13 +0200

    Add files via upload

--- htab_statistics.c
+++ htab_statistics.c
@@ -4,17 +4,14 @@
 // Compiled: gcc 11.4.0
 
 #include <stdio.h>
+#include <stdint.h>
 #include "htab.h"
 #include "htab_structs.h"
 
 // Function that prints hash table statistics to stderr
 void htab_statistics(const htab_t *t) {
-    fprintf(stderr, "Hash table statistics \n");
-    fprintf(stderr, "Bucket count: %zu\n", t->arr_size);
-    fprintf(stderr, "Items in total: %zu\n", t->size);
-
-    double avg_items = (double)t->size / t->arr_size;
-    fprintf(stderr, "Average items in bucket: %.2f\n", avg_items);
+    size_t min_items = SIZE_MAX;
+    size_t max_items = 0;
 
     for (size_t i = 0; i < t->arr_size; ++i) {
         size_t bucket_length = 0;
@@ -24,6 +21,22 @@ void htab_statistics(const htab_t *t) {
             bucket_length++;
             item = item->next;
         }
-        fprintf(stderr, "Bucket %zu: %zu\n", i, bucket_length);
+
+        if (bucket_length > max_items) {
+            max_items = bucket_length;
+        }
+        if (bucket_length < min_items) {
+            min_items = bucket_length;
+        }
+    }
+
+    if (t->size > 0) {
+        double avg_items = (double)t->size / t->arr_size;
+        fprintf(stderr, "Minimum items per bucket: %zu\n", min_items);
+        fprintf(stderr, "Maximum items per bucket: %zu\n", max_items);
+        fprintf(stderr, "Average items per bucket: %.2f\n", avg_items);
+    }
+    else {
+        fprintf(stderr,"Hash table is empty.\n");
     }
 }
